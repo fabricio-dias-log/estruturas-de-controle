4382ffff08b90843171ddd041e185c8925f1e06a
Fabricio Dias <fabricio270909@gmail.com>
2025-02-08 17:30:06 -0300

    Using for part 1

---
+++ for_01.dart
@@ -0,0 +1,15 @@
+void main() {
+  for (int a = 0; a < 10; a++) {
+    print(a);
+  }
+
+  for (int i = 100; i >= 0; i -= 4) {
+    print(i);
+  }
+
+  int b = 0;
+
+  for (; b < 10; b++) {
+    print(b);
+  }
+}
